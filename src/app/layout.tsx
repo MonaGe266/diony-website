fec6628c402513edbed68cb6c9621a70c1fd86e6
yummyyummy <your-email@example.com>
2025-02-27 18:22:17 +0800

    Move files to root directory

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,5 @@
 import './globals.css';
 import type { Metadata } from 'next';
-import { Inter } from 'next/font/google';
-
-const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
   title: '个人作品集 & 笔记',
@@ -16,7 +13,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="zh">
-      <body className={inter.className}>{children}</body>
+      <body>{children}</body>
     </html>
   );
 }
