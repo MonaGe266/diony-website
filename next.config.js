6bacbf626921358ff3f707be33afc29ac40a5e39
yummyyummy <your-email@example.com>
2025-03-04 13:10:52 +0800

    Fix component import paths

--- next.config.js
+++ next.config.js
@@ -1,9 +1,16 @@
 /** @type {import('next').NextConfig} */
+const path = require('path');
+
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   images: {
     unoptimized: true,
+  },
+  webpack: (config) => {
+    config.resolve.alias['@'] = path.join(__dirname, 'src');
+    config.resolve.alias['@/components'] = path.join(__dirname, 'src/app/components');
+    return config;
   }
 }
 
